0136789661b86ff4d956cd93439c42aa8faf23c4
jpedro002 <joaopsilvavolei@gmail.com>
2024-08-28 22:08:32 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import '../constants/glabal.css'
+
+import { Text, View } from 'react-native'
+
+export default function HomeScreen() {
+	return (
+		<View className="flex-1 items-center justify-center bg-white">
+			<Text className="text-blue-500 text-2xl">Hello, NativeWind v4!</Text>
+		</View>
+	)
+}
